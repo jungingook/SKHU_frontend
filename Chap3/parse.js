e7b7d0fdb43f12060408f3ba9b4f6cfe0fcaa05d
JungIngook <wjddlsrnr307@gmail.com>
2020-03-24 18:48:16 +0900

    2_02 javascript 언어 수업

---
+++ Chap3/parse.js
@@ -0,0 +1,15 @@
+let s = "123.456";
+
+let a = parseInt(s);
+console.log(a);
+
+let b = parseFloat(s);
+console.log(b);
+
+let c = Number(s);
+console.log(c);
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+console.log(typeof s);
